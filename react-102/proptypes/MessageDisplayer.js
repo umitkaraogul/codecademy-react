c6aa3f2fb390161bb70130a54bfe029078760bb7
Ümit Karaoğul <ukaraogul@gmail.com>
2018-02-15 17:56:02 +0300

    proptypes

---
+++ react-102/proptypes/MessageDisplayer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MessageDisplayer extends React.Component {
+  render() {
+    return <h1>{this.props.message}</h1>;
+  }
+}
+
+// This propTypes object should have
+// one property for each expected prop:
+MessageDisplayer.propTypes = {
+  message: React.PropTypes.string
+};
---
+++ react-102/proptypes/components/GuineaPigs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class GuineaPigs extends React.Component {
+  render() {
+    let src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+}
+GuineaPigs.propTypes={
+  src:React.PropTypes.string.isRequired
+};
